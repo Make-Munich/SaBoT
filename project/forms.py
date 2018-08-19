69d17188da3346c86ff0086bf0d06330784b307a
derchris <derchris@me.com>
2018-08-19 21:00:56 +0200

    api test

--- project/forms.py
+++ project/forms.py
@@ -89,9 +89,9 @@ class ProjectTalkForm(forms.ModelForm):
 	#talk_user = User.objects.get(email=username)
 
 	def __init__(self, *args, **kwargs):
-		self.user = kwargs.pop('user', None)
+		self.request = kwargs.pop('request', None)
 		#kwargs['instance'] = user
-		print('user: %s' % self.user)
+		print('user: %s' % self.request.user)
 		#super().__init__(*args, **kwargs)
 		#self.talk_user = User.get_username()
 		#self.user = kwargs.pop('user')
