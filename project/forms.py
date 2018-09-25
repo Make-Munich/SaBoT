f3f44870a3d301ea378ef66ca2137a95e17b80a7
derchris <derchris@me.com>
2018-09-25 04:11:41 +0200

    add form for general, project, booth

--- project/forms.py
+++ project/forms.py
@@ -9,71 +9,139 @@ from django.contrib.auth.models import User
 class ProjectGeneralForm(forms.ModelForm):
 	class Meta:
 		model = Project
-		fields = ("projectName", "logo", "homepage")
-
-		widgets = {
-			"logo" : forms.widgets.FileInput,
-		}
+		fields = (
+			"projectName", 
+			"firstname", 
+			"lastname", 
+			"email", 
+			"phone", 
+			"homepage", 
+			"projecttype", 
+			"language", 
+			"hear", 
+			"recommendation",
+			"generalComment"
+			)
 
 	def __init__(self, *args, **kwargs):
 		super(ProjectGeneralForm, self).__init__(*args, **kwargs)
 		self.helper = FormHelper()
-		if self.instance and self.instance.logo:
-			self.helper.layout = Layout(
-				Field("projectName"),
-				Field("logo"),
-				Div(
-					HTML("<p>Current logo:</p><img src=\"{{object.logo.url}}\" style=\"max-height:200px\"/>"),
-					css_class = "control-group"),
-				Field("homepage"),
-#				FormActions(Submit("Save", "Save changes"))
+		self.helper.layout = Layout(
+			Field("projectName"),
+			Field("firstname"),
+			Field("lastname"),
+			Field("email"),
+			Field("phone"),
+			Field("homepage"),
+			Field("projecttype"),
+			Field("language"),
+			Field("hear"),
+			Field("recommendation"),
+			Field("generalComment"),
+#			FormActions(Submit("Save", "Save changes"))
 			)
-		else:
-			self.helper.layout = Layout(
-				Field("projectName"),
-				Div(
-					Div(Field("logo"),css_class = "col-md-2"),
-					css_class = "row"
-				),
-				Field("homepage"),
-#				FormActions(Submit("Save", "{% if object %}Save changes{% else %}Register{% endif %}"))
-			)
-
 		if self.instance is not None and self.instance.id is not None:
 			self.helper.add_input(Submit("Save", "Save changes"))
 		else:
 			self.helper.add_input(Submit("Save", "Register"))
 
+		
+
 class ProjectDescriptionForm(forms.ModelForm):
 	class Meta:
 		model = Project
-		fields = ("descriptionDE", "descriptionEN")
+		fields = (
+			"descriptionDE", 
+			"descriptionEN",
+			"projectArea",
+			"logoOrg",
+			"logo", 
+			"logoTeam",
+			"video"
+			)
+
+		widgets = {
+			"logoOrg" : forms.widgets.FileInput,
+			"logo" : forms.widgets.FileInput,
+			"logoTeam" : forms.widgets.FileInput,
+		}
 
 	def __init__(self, *args, **kwargs):
 		super(ProjectDescriptionForm, self).__init__(*args, **kwargs)
 		self.helper = FormHelper()
-		self.helper.layout = Layout(
-			Field("descriptionDE"),
-			Field("descriptionEN"),
+		if self.instance and self.instance.logo and self.instance.logoOrg and self.instance.logoTeam:
+			self.helper.layout = Layout(
+				Field("descriptionDE"),
+				Field("descriptionEN"),
+				Field("projectArea"),
+				Field("logoOrg"),
+					Div(
+						HTML("<p>Current logo:</p><img src=\"{{object.logoOrg.url}}\" style=\"max-height:200px\"/>"),
+						css_class = "control-group"),
+				Field("logo"),
+					Div(
+						HTML("<p>Current logo:</p><img src=\"{{object.logo.url}}\" style=\"max-height:200px\"/>"),
+						css_class = "control-group"),
+				Field("logoTeam"),
+					Div(
+						HTML("<p>Current logo:</p><img src=\"{{object.logoTeam.url}}\" style=\"max-height:200px\"/>"),
+						css_class = "control-group"),
+				Field("video"),
 #			FormActions(Submit("Save", "Save changes"))
-		)
-		self.helper.add_input(Submit("Save","Save changes"))
+			)
+		else:
+			self.helper.layout = Layout(
+				Field("descriptionDE"),
+				Field("descriptionEN"),
+				Field("projectArea"),
+				Div(
+					Div(Field("logoOrg"),css_class = "col-md-2"),
+					css_class = "row"
+				),
+				Div(
+					Div(Field("logo"),css_class = "col-md-2"),
+					css_class = "row"
+				),
+				Div(
+					Div(Field("logoTeam"),css_class = "col-md-2"),
+					css_class = "row"
+				),
+				Field("video"),self.helper.add_input(Submit("Save","Save changes"))
+			)
+		if self.instance is not None and self.instance.id is not None:
+			self.helper.add_input(Submit("Save", "Save changes"))
+		else:
+			self.helper.add_input(Submit("Save", "Register"))
+
 
 class ProjectBoothForm(forms.ModelForm):
 	class Meta:
 		model = Project
-		fields = ("boothPreferedLocation", "boothNumTables", "boothNumChairs", "boothPower", "boothArea", "boothComment")
+		fields = (
+			"boothDescription", 
+			"boothTables",
+			"boothChairs",
+			"boothBenches",
+			"boothPower",
+			"boothExtras",
+			"boothExtrasComment",
+			"boothOwn",
+			"boothSafety"
+			)
 
 	def __init__(self, *args, **kwargs):
 		super(ProjectBoothForm, self).__init__(*args, **kwargs)
 		self.helper = FormHelper()
 		self.helper.layout = Layout(
-			Field("boothPreferedLocation"),
-			Field("boothNumTables"),
-			Field("boothNumChairs"),
-            Field("boothPower"),
-			Field("boothArea"),
-			Div("boothComment", HTML("{% if user.is_staff %} <p>Admin only</p> {% endif %}")),
+			Field("boothDescription"),
+			Field("boothTables"),
+			Field("boothChairs"),
+            Field("boothBenches"),
+			Field("boothPower"),
+			Field("boothExtras"),
+			Field("boothExtrasComment"),
+			Field("boothOwn"),
+			Field("boothSafety"),
 #			FormActions(Submit("Save", "Save changes"))
 		)
 		self.helper.add_input(Submit("Save","Save changes"))
